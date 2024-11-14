73ad89447292736ebe60a26aa74cdc40bfe48d2a
ahmed mecherouk <ahmedmecherouk@gmail.com>
2024-11-14 19:20:15 +0100

    Watch list

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,29 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Movie.destroy_all
+
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+
+
+
+# require "open-uri"
+# require "json"
+# movies = JSON.parse(URI.parse("https://tmdb.lewagon.com/movie/top_rated").read)
+# number = 0
+# puts "Destroying DB"
+# Movie.destroy_all
+# puts "Seeding starts"
+# 15.times do
+#   number += 1
+#   movie = Movie.create(
+#     title: movies["results"][number]["original_title"],
+#     overview: movies["results"][number]["overview"],
+#     poster_url: "https://image.tmdb.org/t/p/w500#{movies["results"][number]["poster_path"]}",
+#     rating: movies["results"][number]["vote_average"].round(1),
+#   )
+# end
+# puts "Seeding done"
